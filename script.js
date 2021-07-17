f4ece3e33167555ddbd7490a7a8c777579cc6954
lil-sahil <sahilnagpal93@yahoo.ca>
2021-07-17 14:40:07 -0400

    Logic for data aquisition from form completed.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const bookTitle = document.querySelector('#title-field > input');
+const bookAuthor = document.querySelector('#author-field > input');
+const bookPage = document.querySelector('#page-field > input');
+const bookRead = document.querySelectorAll('#read-field > input');
+
+const submitButton = document.querySelector('form > .submit-btn');
+
+console.log(bookPage.value);
+
 // Book array as defined by the user;
 let bookArray = []
 
@@ -18,25 +27,92 @@ function addBookToLibrary(title, author, pages, read){
 
 
 
-
-const bookTitle = document.querySelector('#title-field > input');
-
-const button = document.querySelector('form > .submit-btn');
+// Form data aquisition---------
 
 
-button.addEventListener("click",myFunc);
+submitButton.addEventListener("click",myFunc);
 
 function myFunc(e){
   e.preventDefault();
   let title = bookTitle.value;
-  let author = "jane doe";
-  let pages = 23;
-  let read = true;
+  let author = bookAuthor.value;
+  let pages = bookPage.value;
+  let read = '';
+  
+  bookRead.forEach(option => {
+    if (option.checked){
+      read = option.value;
+    };
+  });
 
-  addBookToLibrary(title, author, pages, read);
+  // Validate the data
+
+  if (dataValidation(title, author, pages, read)){
+
+    addBookToLibrary(title, author, pages, read);
+
+    clearFromFields();
+  }
+
+  else {
+    return false;
+  }
+}
+
+
+// Form data validation
+
+function dataValidation(title, author, pages, read){
+
+  let focusFields = [];
+  // title
+  if (title.length < 1){
+    focusFields.push(bookTitle);
+  }
+
+  // author
+  if (author.length < 1){
+    focusFields.push(bookAuthor);
+  }
+
+  // read
+  if (read.length < 1){
+    focusFields.push(bookRead[0]);
+  }
+
+  // pages
+  if (pages <= 0){
+    focusFields.push(bookPage);
+  }
+
+  if (focusFields.length === 0){
+    return true;
+  }
+
+  else {
+    focus(focusFields);
+  }
 
 }
 
+// Focus Field with missing data
+function focus(arr){
+  arr.forEach(field => field.focus());
+}
+
+// Clear form field
+function clearFromFields(){
+  bookTitle.value = "";
+  bookAuthor.value = "";
+  bookPage.value = "";
+  bookRead.forEach(option => option.checked = false);
+}
+
+// --------------------------------
+
+
+
+// Display items in bookarray as flex items
 
 
 const seeButton = document.querySelector('.see-btn');
